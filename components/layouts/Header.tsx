6db81eeeafc578da0404f22c0ff8c8cc26aa5a85
local-Leee <locallee.c@gmail.com>
2025-04-20 23:55:39 +0900

    5강. 중첩 레이아웃(공통 UI 루트 레이아웃 정의)

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -4,9 +4,9 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 border-b">
       <div className="container mx-auto flex h-14 items-center px-4">
-        <Link href="/" className="text-xl font-semibold">
+        <a href="#" className="text-xl font-semibold">
           <span className="font-bold">현지인 블로그</span>
-        </Link>
+        </a>
         <nav className="ml-auto flex items-center gap-4">
           <Link href="/" className="hover:text-primary font-medium">
             홈
