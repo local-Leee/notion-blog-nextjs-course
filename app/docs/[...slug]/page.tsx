431f8bcd786af8834f5b3ba6c646825677f8e2a1
local-Leee <locallee.c@gmail.com>
2025-04-21 00:56:55 +0900

    5. 선택적 Catch-all

--- app/docs/[...slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-interface DocsPageProps {
-  params: Promise<{ slug: string[] }>;
-}
-
-export default async function DocsPage({ params }: DocsPageProps) {
-  const slug = await params;
-  console.log(slug);
-  return <div>DocsPage</div>;
-}
