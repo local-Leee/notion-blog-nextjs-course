c038b6b744ddc62ef690074abceb544fd16bbd98
local-Leee <locallee.c@gmail.com>
2025-05-06 20:54:34 +0900

    10. 새고해도 정렬select박스에 현재 선택한 정렬 유지되게 적용

--- app/_components/SortSelect.tsx
+++ app/_components/SortSelect.tsx
@@ -8,12 +8,17 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 export const SortSelect = () => {
   console.log('SortSelect');
 
   // useRouter 정렬 선택 시 url을 업데이트하기 위해 사용
   const router = useRouter();
+
+  //useSearchParams 훅을 이용해서 현재 url의 sort 파라미터를 가져온다.
+  const searchParams = useSearchParams();
+  // default 설정: sort 파라미터가 없으면 'latest'로 설정
+  const sort = searchParams.get('sort') || 'latest';
 
   // 이벤트 핸들러 함수 정의
   const handleSort = (value: string) => {
@@ -26,7 +31,8 @@ export const SortSelect = () => {
   return (
     // onValueChange props를 이용해서 이벤트리스너를 작성할 수 있다.
     // 이벤트 핸들러로 handleSort 함수를 전달한다.
-    <Select defaultValue="latest" onValueChange={handleSort}>
+    // value 속성에 sort 파라미터를 바인딩한다.
+    <Select value={sort} defaultValue="latest" onValueChange={handleSort}>
       <SelectTrigger className="w-[180px]">
         <SelectValue placeholder="정렬 방식 선택" />
       </SelectTrigger>
