824ca0bce1b9f47ac91ac5829e6db10eb8450345
local-Leee <locallee.c@gmail.com>
2025-05-06 20:41:31 +0900

    10. 정렬 선택 시 useRouter() 사용하여 url 업데이트

--- app/_components/SortSelect.tsx
+++ app/_components/SortSelect.tsx
@@ -8,11 +8,25 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
-
+import { useRouter } from 'next/navigation';
 export const SortSelect = () => {
   console.log('SortSelect');
+
+  // useRouter 정렬 선택 시 url을 업데이트하기 위해 사용
+  const router = useRouter();
+
+  // 이벤트 핸들러 함수 정의
+  const handleSort = (value: string) => {
+    console.log('value: ', value);
+
+    // router의 push 메서드를 이용해서 정렬 선택 시 url을 업데이트
+    router.push(`?sort=${value}`);
+  };
+
   return (
-    <Select defaultValue="latest">
+    // onValueChange props를 이용해서 이벤트리스너를 작성할 수 있다.
+    // 이벤트 핸들러로 handleSort 함수를 전달한다.
+    <Select defaultValue="latest" onValueChange={handleSort}>
       <SelectTrigger className="w-[180px]">
         <SelectValue placeholder="정렬 방식 선택" />
       </SelectTrigger>
