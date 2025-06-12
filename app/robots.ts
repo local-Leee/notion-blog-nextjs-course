aaffb595ff184d7d24a5740ec8ef403b505f3b17
local-Leee <locallee.c@gmail.com>
2025-06-12 14:26:52 +0900

    17-2. Metadata Files - env 환경변수 다시적용!!

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/admin/'],
     },
-    sitemap: 'https://nblog-nextjs-course.vercel.app/sitemap.xml',
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
   };
 }
